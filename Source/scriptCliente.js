7738c554e601d37c864bb3dc02b6086aa6e3fcb7
IMvErickz <erickspy2003@gmail.com>
2022-07-11 21:42:50 -0300

    added the second conditions of the inputs

--- Source/scriptCliente.js
+++ Source/scriptCliente.js
@@ -13,22 +13,47 @@ function funcoes() {
 
 function gravar() {
     let nome = document.getElementById("first_name").value
+    if (nome == "") {
+        swal("O campo nome está vazio", "Por favor digite novamente", "error")
+        return 0
+        
+    }
     localStorage.setItem("Nome", nome)
 
      let sobrenome = document.getElementById("last_name").value
     localStorage.setItem("Sobrenome", sobrenome)
+    if (sobrenome == "") {
+    swal("O campo sobrenome está vazio", "Por favor digite novamente", "error")
+    return 0
+}
     
      let cpf = document.getElementById("cpf").value
     localStorage.setItem("CPF", cpf)
+    if (cpf == "") {
+    swal("O campo CPF está vazio", "Por favor digite novamente", "error")
+    return 0
+}
 
      let telefone = document.getElementById("phone").value
     localStorage.setItem("Phone", telefone)
+    if (telefone == "") {
+    swal("O campo telefone está vazio", "Por favor digite novamente", "error")
+    return 0
+}
     
      let dataNasc = document.getElementById("nascimento").value
     localStorage.setItem("Nasc", dataNasc)
+    if (dataNasc == "") {
+    swal("O campo de nascimento está vazio", "Por favor digite novamente", "error")
+    return 0
+}
     
      let email = document.getElementById("email").value
     localStorage.setItem("Email", email)
+    if (email == "") {
+    swal("O campos email está vazio", "Por favor digite novamente", "error")
+    return 0
+}
     
      let senha = document.getElementById("password").value
     localStorage.setItem("Senha", senha)
@@ -36,26 +61,20 @@ function gravar() {
      let confSenha = document.getElementById("passwordConf").value
     localStorage.setItem("Conf", confSenha)
 
-    let inputs = [nome, sobrenome, cpf, telefone, dataNasc, email, senha, confSenha]
-
-    if (inputs = []) {
-        swal("Os campos estão vazios","Por favor digite novamente", "error")
-    }
-
-    if (senha != confSenha) {
+     if (senha != confSenha) {
         swal("Senhas não conferem","Por favor digite novamente", "error")
         let remove = localStorage.removeItem("Senha")
         let removeconf = localStorage.removeItem("Conf")
     }
 
-    if (senha.length < 8) {
+    else if (senha.length < 8) {
         swal("No mínimo 8 caractéres", "Por favor digite novamente", "error")
         let remove = localStorage.removeItem("Senha")
         let removeconf = localStorage.removeItem("Conf")
         
     } else {
         swal("Parabéns", "Gravado com sucesso", "success")
-        window.location.href = "/Pages/infoCadastro.html"
+        
     }
 
 }
@@ -93,6 +112,7 @@ function limpar() {
 
  let senha = document.getElementById("password").value = ""
 
- let confSenha = document.getElementById("passwordConf").value = ""
+    let confSenha = document.getElementById("passwordConf").value = ""
+    window.location.href = "/Pages/infoCadastro.html"
 
 }
