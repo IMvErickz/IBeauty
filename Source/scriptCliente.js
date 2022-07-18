d20ecec69c9fdd98cd274d6c3b9617f9572a6e38
IMvErickz <erickspy2003@gmail.com>
2022-07-17 21:10:59 -0300

    modified the variables and added a test function for object

--- Source/scriptCliente.js
+++ Source/scriptCliente.js
@@ -11,47 +11,108 @@ function funcoes() {
     limpar()   
 }
 
+//Teste de objeto
+function gravaar() {
+    const cliente = [{
+        Nome: document.getElementById("first_name").value,
+        Sobrenome: document.getElementById("last_name").value,
+        Cpf: document.getElementById("cpf").value,
+        Telefone: document.getElementById("phone").value,
+        Nascimento: document.getElementById("nascimento").value,
+        Email: document.getElementById("email").value,
+        Senha: document.getElementById("password").value,
+        ConfSenha: document.getElementById("passwordConf").value
+    }]
+
+    localStorage.setItem("Cliente", JSON.stringify(cliente))
+    let rec = JSON.parse(localStorage.getItem("Cliente"))
+
+    console.log(rec)
+
+    //let keep 
+
+    //keep.push(cliente)
+    function isEmpty(obj) {
+        return Object.keys(obj).length === 0;
+    }
+    console.log(isEmpty({ cliente }))
+
+    function ObjCompare(obj1, obj2){
+    if (obj1 === obj2){
+        return true;
+    };
+    return false;
+}
+    //console.log(ObjCompare(cliente, []))
+  
+    if (isEmpty(cliente)) {
+        swal("Os campos estão vazios", "Por favor digite novamente", "error")
+        .then(() => {
+                let remove = localStorage.removeItem("Cliente")
+            
+        })
+    } 
+      
+    
+
+    else if(cliente.Senha != cliente.ConfSenha){
+        swal("As senhas não conferem", "Por favor digite novamente", "error")
+            .then(() => {
+            let remove = localStorage.removeItem("Cliente")
+        })
+    } else {
+        swal("Parabéns", "Gravado com sucesso", "success")
+        
+    }
+}
 function gravar() {
     let nome = document.getElementById("first_name").value
+    localStorage.setItem("Nome", nome)
     if (nome == "") {
         swal("O campo nome está vazio", "Por favor digite novamente", "error")
+        let remove = localStorage.removeItem("Nome")
         return 0
         
     }
-    localStorage.setItem("Nome", nome)
+    
 
      let sobrenome = document.getElementById("last_name").value
     localStorage.setItem("Sobrenome", sobrenome)
     if (sobrenome == "") {
-    swal("O campo sobrenome está vazio", "Por favor digite novamente", "error")
+        swal("O campo sobrenome está vazio", "Por favor digite novamente", "error")
+        let remove = localStorage.removeItem("Sobrenome")
     return 0
 }
     
      let cpf = document.getElementById("cpf").value
     localStorage.setItem("CPF", cpf)
     if (cpf == "") {
-    swal("O campo CPF está vazio", "Por favor digite novamente", "error")
+        swal("O campo CPF está vazio", "Por favor digite novamente", "error")
+        let remove = localStorage.removeItem("CPF")
     return 0
 }
 
      let telefone = document.getElementById("phone").value
     localStorage.setItem("Phone", telefone)
     if (telefone == "") {
-    swal("O campo telefone está vazio", "Por favor digite novamente", "error")
+        swal("O campo telefone está vazio", "Por favor digite novamente", "error")
+        let remove = localStorage.removeItem("Phone")
     return 0
 }
     
      let dataNasc = document.getElementById("nascimento").value
-    localStorage.setItem("Nasc", dataNasc)
+    localStorage.setItem("Nascimento", dataNasc)
     if (dataNasc == "") {
-    swal("O campo de nascimento está vazio", "Por favor digite novamente", "error")
+        swal("O campo de nascimento está vazio", "Por favor digite novamente", "error")
+        let remove = localStorage.removeItem("Nascimento")
     return 0
 }
     
      let email = document.getElementById("email").value
     localStorage.setItem("Email", email)
     if (email == "") {
-    swal("O campos email está vazio", "Por favor digite novamente", "error")
+        swal("O campo email está vazio", "Por favor digite novamente", "error")
+        let remove = localStorage.removeItem("Email")
     return 0
 }
     
