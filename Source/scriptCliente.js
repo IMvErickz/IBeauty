1c8a12351556e926adf515f38870a3a9f45b1650
IMvErickz <erickspy2003@gmail.com>
2022-07-18 18:00:48 -0300

    changed de LocalStorage for a Client Object

--- Source/scriptCliente.js
+++ Source/scriptCliente.js
@@ -12,8 +12,8 @@ function funcoes() {
 }
 
 //Teste de objeto
-function gravaar() {
-    const cliente = [{
+function gravar() {
+    const cliente = {
         Nome: document.getElementById("first_name").value,
         Sobrenome: document.getElementById("last_name").value,
         Cpf: document.getElementById("cpf").value,
@@ -22,16 +22,12 @@ function gravaar() {
         Email: document.getElementById("email").value,
         Senha: document.getElementById("password").value,
         ConfSenha: document.getElementById("passwordConf").value
-    }]
+    }
 
     localStorage.setItem("Cliente", JSON.stringify(cliente))
     let rec = JSON.parse(localStorage.getItem("Cliente"))
-
     console.log(rec)
-
-    //let keep 
-
-    //keep.push(cliente)
+    
     function isEmpty(obj) {
         return Object.keys(obj).length === 0;
     }
@@ -45,103 +41,24 @@ function gravaar() {
 }
     //console.log(ObjCompare(cliente, []))
   
-    if (isEmpty(cliente)) {
+    if (cliente.Email == "" || cliente.Cpf == "" || cliente.Senha == "") {
         swal("Os campos estão vazios", "Por favor digite novamente", "error")
         .then(() => {
-                let remove = localStorage.removeItem("Cliente")
+            let remove = localStorage.removeItem("Cliente")
             
         })
-    } 
-      
-    
-
-    else if(cliente.Senha != cliente.ConfSenha){
+    } else if(cliente.Senha != cliente.ConfSenha){
         swal("As senhas não conferem", "Por favor digite novamente", "error")
             .then(() => {
             let remove = localStorage.removeItem("Cliente")
         })
     } else {
         swal("Parabéns", "Gravado com sucesso", "success")
+            .then(() => {
+            window.location.href = "/Pages/infoCadastro.html"
+        })
         
     }
-}
-function gravar() {
-    let nome = document.getElementById("first_name").value
-    localStorage.setItem("Nome", nome)
-    if (nome == "") {
-        swal("O campo nome está vazio", "Por favor digite novamente", "error")
-        let remove = localStorage.removeItem("Nome")
-        return 0
-        
-    }
-    
-
-     let sobrenome = document.getElementById("last_name").value
-    localStorage.setItem("Sobrenome", sobrenome)
-    if (sobrenome == "") {
-        swal("O campo sobrenome está vazio", "Por favor digite novamente", "error")
-        let remove = localStorage.removeItem("Sobrenome")
-    return 0
-}
-    
-     let cpf = document.getElementById("cpf").value
-    localStorage.setItem("CPF", cpf)
-    if (cpf == "") {
-        swal("O campo CPF está vazio", "Por favor digite novamente", "error")
-        let remove = localStorage.removeItem("CPF")
-    return 0
-}
-
-     let telefone = document.getElementById("phone").value
-    localStorage.setItem("Phone", telefone)
-    if (telefone == "") {
-        swal("O campo telefone está vazio", "Por favor digite novamente", "error")
-        let remove = localStorage.removeItem("Phone")
-    return 0
-}
-    
-     let dataNasc = document.getElementById("nascimento").value
-    localStorage.setItem("Nascimento", dataNasc)
-    if (dataNasc == "") {
-        swal("O campo de nascimento está vazio", "Por favor digite novamente", "error")
-        let remove = localStorage.removeItem("Nascimento")
-    return 0
-}
-    
-     let email = document.getElementById("email").value
-    localStorage.setItem("Email", email)
-    if (email == "") {
-        swal("O campo email está vazio", "Por favor digite novamente", "error")
-        let remove = localStorage.removeItem("Email")
-    return 0
-}
-    
-     let senha = document.getElementById("password").value
-    localStorage.setItem("Senha", senha)
-
-     let confSenha = document.getElementById("passwordConf").value
-    localStorage.setItem("Conf", confSenha)
-
-     if (senha != confSenha) {
-        swal("Senhas não conferem","Por favor digite novamente", "error")
-        let remove = localStorage.removeItem("Senha")
-        let removeconf = localStorage.removeItem("Conf")
-    }
-
-    else if (senha.length < 8) {
-        swal("No mínimo 8 caractéres", "Por favor digite novamente", "error")
-        let remove = localStorage.removeItem("Senha")
-        let removeconf = localStorage.removeItem("Conf")
-        
-    } else {
-         swal("Parabéns", "Gravado com sucesso", "success")
-             .then(() => {
-             window.location.href = "/Pages/infoCadastro.html"
-         })
-         
-    }
-   
-
 }
 
 function mostrar() {
