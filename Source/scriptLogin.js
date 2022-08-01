618dca7a6c2464d8dbfd708b47b29c9e830796c1
IMvErickz <erickspy2003@gmail.com>
2022-08-01 10:20:43 -0300

    added the funtion of login

--- Source/scriptLogin.js
+++ Source/scriptLogin.js
@@ -1,3 +1,5 @@
+//const { default: swal } = require("sweetalert")
+
 window.onload = function () {
     let botao = document.getElementById("buttonEnviar")
     botao.addEventListener("click", funcoes)
@@ -6,24 +8,25 @@ window.onload = function () {
 function funcoes() {
     gravar()
     mostrar()
-    limpar()
 }
 
 function gravar() {
-    let email = document.getElementById("email").value
-    localStorage.setItem("Email", email)
+  let inputEmail = document.getElementById("email").value
+  let email = JSON.parse(localStorage.getItem("Cliente"))
+  
+  let inputPass = document.getElementById("password").value
+  let password = JSON.parse(localStorage.getItem("Cliente"))
 
-    let senha = document.getElementById("password").value
-    localStorage.setItem("Senha", senha)
-}
-
-function mostrar() {
-    let recEmail = localStorage.getItem("Email")
-    console.log(recEmail)
-
-    let recSenha = localStorage.getItem("Senha")
-    console.log(recSenha)
-}
+  if (email.Email == inputEmail || password.Senha == inputPass) {
+    swal("Logado", "Dados corretos", "success")
+  } else {
+    swal("Dados incorretos", "Por favor tente novamente", "error")
+      .then(() => {
+      limpar()
+    })
+  }
+    
+  }
 
 function limpar() {
     let email = document.getElementById("email").value = ""
@@ -32,17 +35,17 @@ let senha = document.getElementById("password").value = ""
 
 }
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
+//function onSignIn(googleUser) {
+  //var profile = googleUser.getBasicProfile();
+ // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+ // console.log('Name: ' + profile.getName());
+ // console.log('Image URL: ' + profile.getImageUrl());
+  //console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+//}
 
- function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
+ //function signOut() {
+   // var auth2 = gapi.auth2.getAuthInstance();
+   // auth2.signOut().then(function () {
+    //  console.log('User signed out.');
+   // });
+ // }
