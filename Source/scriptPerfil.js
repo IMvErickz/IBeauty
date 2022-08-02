ab442a3c85c98c7c3fbb9432097a9801bb9bb7cb
IMvErickz <erickspy2003@gmail.com>
2022-08-02 10:39:05 -0300

    modified the message for logout

--- Source/scriptPerfil.js
+++ Source/scriptPerfil.js
@@ -1,6 +1,6 @@
 let button = document.getElementById("quit")
     .addEventListener("click", () => {
-        swal("Deslogado", "saída com sucesso", "success")
+        swal("Até mais", "saída com sucesso", "success")
             .then(() => {
             window.location.href = "/Pages/Login.html"
         })
