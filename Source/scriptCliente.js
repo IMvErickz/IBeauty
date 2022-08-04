5d307411989691a653969a62a659bed3553cd1f1
unknown <erickspy2003@gmail.com>
2022-08-04 18:29:11 -0300

    added a mask for input cpf

--- Source/scriptCliente.js
+++ Source/scriptCliente.js
@@ -67,3 +67,8 @@ function gravar() {
         
     }
 }
+
+ document.getElementById('cpf').addEventListener('input', function (e) {
+      var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/);
+      e.target.value = x[1] + '.' + x[2] + '.' + x[3] + '-' + x[4];
+    });
